34623a0466de1943eb704efd1af9c7e28ab59d37
fumiama <s862105088@gmail.com>
2021-09-17 13:51:27 +0800

    a

--- link/init.go
+++ link/init.go
@@ -103,7 +103,7 @@ func UpgradeLink(w http.ResponseWriter, r *http.Request, adviceip uint64) (uint6
 func ListenAccess() error {
 	listener, err := net.Listen("tcp", myhost)
 	if err == nil {
-		log.Infof("[link] listen access on", myhost)
+		log.Infoln("[link] listen access on", myhost)
 		go log.Fatal(http.Serve(listener, nil))
 	}
 	return err
